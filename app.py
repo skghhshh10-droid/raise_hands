da4e74a65bded07838c982eae65f39368e4b92e0
skghhshh10-droid <skghhshh10@gmail.com>
2026-06-22 21:12:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ from yolo.detector import detect_hand_raise
 from database.attendance import save_attendance
 from dashboard.statistics import attendance_count
 from ui.main_page import show_dashboard
-# 메인 실행 파일
+
 
 def main():
-    pass
+
+    frame = None
+
+    if detect_hand_raise(frame):
+        save_attendance("학생1")
+
+    count = attendance_count()
+
+    show_dashboard(count)
 
 
 if __name__ == "__main__":
