bbfdf7ed4e76265a2fe647e34ef3acb8e88212a7
joipui213 <joipui213@gmail.com>
2026-06-22 18:29:27 +0900

    Add files via upload

---
+++ dashboard/ex_/pose_detector.py
@@ -0,0 +1,15 @@
+def get_status():
+
+    return "QUESTION"
+
+# 추후 yolo 연결
+
+#def get_status():
+
+#    if hand_raised:
+#        return "QUESTION"
+
+#    if not_focused:
+#        return "NOT_FOCUS"
+
+ #   return "NORMAL"
