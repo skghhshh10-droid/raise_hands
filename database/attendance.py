8ff4a7ca2db18bda9fa2f11b97c8269e60776be7
skghhshh10-droid <skghhshh10@gmail.com>
2026-06-23 15:32:14 +0900

    Update attendance.py

--- database/attendance.py
+++ database/attendance.py
@@ -52,14 +52,9 @@ def register_student(student_id, name):
 # =========================================================
 
 def save_attendance(name):
-    """
-    학생 이름으로 Students 테이블 조회 후
-    attendance 테이블에 출석 저장
-    """
 
     try:
 
-        # 학생 조회
         student_query = (
             supabase
             .table("Students")
@@ -68,18 +63,15 @@ def save_attendance(name):
             .execute()
         )
 
-        print("선택 이름:", name)
-        print("학생 조회 결과:", student_query.data)
+        print("학생 조회:", student_query.data)
 
         if len(student_query.data) == 0:
-            print("학생이 등록되어 있지 않습니다.")
             return False
 
         student_id = student_query.data[0]["student_id"]
 
         print("학번:", student_id)
 
-        # 중복 출석 확인
         check = (
             supabase
             .table("attendance")
@@ -88,31 +80,12 @@ def save_attendance(name):
             .execute()
         )
 
-        print("기존 출석 데이터:", check.data)
+        print("출석조회:", check.data)
 
         if len(check.data) > 0:
-            print("이미 출석한 학생입니다.")
             return False
 
-        data = {
-            "student_id": student_id,
-            "status": "출석"
-        }
-
-        supabase.table(
-            "attendance"
-        ).insert(data).execute()
-
-        print("출석 저장 성공")
-
-        return True
-
-    except Exception as e:
-
-        print("ATTENDANCE ERROR :", e)
-
-        return False
-
+        ...
 
 # =========================================================
 # 출석 조회
